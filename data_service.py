30fb124adf7359ec985921e362c2b21a4b4494f2
Sergey <sergey@MacBookSergey.local>
2020-09-23 23:16:12 +0300

    створена иункція для роботи з довідником

--- data_service.py
+++ data_service.py
@@ -4,6 +4,26 @@
 # файла оперативної інформ.
 # -------------------------------------
 
-def get_clients():
-    clients = [
+enterprises = [
+    "1010; Універмаг;  1704",
+    "1020; Дружба ЛТД; 972",
+    "1030; Радунь;     500"
     ]
+
+
+indicators = [
+
+]
+
+
+def get_clients():
+    enterprises_list = []
+    for enterprise in enterprises:
+        enterprises_list.append(enterprise.split(";"))
+
+    print(enterprises_list)
+    pass
+ 
+
+
+# get_clients()
