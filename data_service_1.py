e18685b7bd18fd74d24071097ac1e330e0da3f03
student-KNTEU <student.knteu@gmail.com>
2020-10-19 14:22:54 +0300

    add list in the data_service

---
+++ data_service_1.py
@@ -0,0 +1,15 @@
+def get_clients():
+ 
+     from_file = [
+     "fasfasfasfs",
+     "fsafasfsafaa"
+     ]
+ 
+    
+      
+        
+
+     return from_file
+
+clients = get_clients()  
+print(clients)
