19a236ee2932f28841327f5e62fee79b6d81acae
sergei8 <shkliarskiy@gmail.com>
2020-10-27 18:22:18 +0200

    get & show

--- data_service.py
+++ data_service.py
@@ -1,11 +1,11 @@
 """модуль зчитує первинні файли для обробки
 """
 
-def get_cliets():
-    """повернає список клієнтів
+def get_clients():
+    """повертає список клієнтів та фороматує значення
 
     Returns:
-        [list]: список клієнтів
+        clients_list: список клієнтів
     """
 
     from_file = [
@@ -19,10 +19,39 @@ def get_cliets():
         "67;ЕЛЬДОРАДО;вул.Васильківська 5"
         ]
 
+    # накопичувач клієнтів
+    clients_list = []
 
-    return from_file
+    # блок виділяє окремі реквізити з кожного рядка
+    for line in from_file:
+        line_list = line.split(';')
+        clients_list.append(line_list)
+
+    return clients_list
 
 
-clients = get_cliets()
-print(clients)   
+def show_clients(clients):
+    """ виводить список клітєнтів за деякою умовою
+
+    Args:
+        clients ([list]): список клітєнтів
+    """
+
+    client_code_from = input("З якого коду клієнта? ")
+    client_code_to   = input("По який код клієнта? ")
+    
+    kol_lines = 0
+
+    for client in clients:
+        if  client_code_from  <= client[0] <= client_code_to:
+            print("код: {:4} назва: {:16} адреса: {:20}".format(client[0], client[1], client[2]))
+            kol_lines = kol_lines + 1
+
+    if kol_lines == 0:
+        print("Записів з кодом {} не знайдено".format(client_code_from))
+
+
+clients = get_clients()
+show_clients(clients)
+
 
