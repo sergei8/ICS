4b86faa5a7856fd3038e1884815f4cbd05ffad59
sergei8 <shkliarskiy@gmail.com>
2020-10-30 08:40:15 +0200

    task 3

--- data_service.py
+++ data_service.py
@@ -1,4 +1,4 @@
-""" модуль для отримання даних про товарообіг
+""" модуль для отримання даних про постачання та вивід їх на екран
 """
 
 
@@ -20,13 +20,51 @@ def get_clients():
         "67;ЕЛЬДОРАДО;вул.Васильківська 5"
     ]
 
-    return from_file
+    # накопичувач клієнтів
+    clients_list = []
+
+    for line in from_file:
+        line_list = line.split(';')
+        clients_list.append((line_list))
+
+    return clients_list
 
 def get_orders():
-    pass
 
-clients_list = get_clients()
+        from_file = [
+        "35;202;ARTLINE Gaming X51 ;2;5200",
+        "39;203;Everest Home 4070 ;1;12000",
+        "44;205;Asus ROG Strix;2;17000",
+        "45;207;MacBook Pro 15”;2;30000",
+        "47;211;Everest Home 4070 ;1;12000",
+        "50;204;MacBook Pro 15”;1;30000",
+        "54;206;Asus ROG Strix;2;17000",
+        "67;212;MacBook Pro 13”;2;26000"
+    ]
 
-for line in clients_list:
-    print(line)
+def show_clients(clients):
+    """виводить список клієнтів
 
+    Args:
+        clients (list): список клієнтів
+    """
+
+    # задати інтервал виводу
+    client_code_from = input("З якого кода клієнта? ")
+    client_code_to   = input("По який код клієнта? ")
+    
+    # накопичує кількість виведених рядків
+    kol_lines = 0
+
+    for client in clients:
+        if  client_code_from  <= client[0] <= client_code_to:
+            print("код: {:3} назва: {:16} адреса: {:20}".format(client[0], client[1], client[2]))
+            kol_lines += 1
+
+    # перевірити чи був вивід хоча б одного рядка
+    if kol_lines == 0:
+        print("По вашому запиту клієнтів не знайдено!")
+    
+
+clients = get_clients()
+show_clients(clients)
