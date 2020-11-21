1a6c02f66f083ddd7dd53b6f1c76a5fa0cbb8889
student-KNTEU <student.knteu@gmail.com>
2020-11-21 20:08:05 +0200

    process_data in work"

--- data_service.py
+++ data_service.py
@@ -2,49 +2,89 @@
 - зчитування та вивід на екран
 """
 
+"""модуль зчитує первинні файли для обробки
+"""
 
 def get_clients():
-    """ повертає список клієнтів який отримує з файлу 'clients.txt`
+    """повертає список клієнтів b з файла `clients.txt`
 
     Returns:
-        client_list: список клієнтів 
-    """    
+        clients_list: список клієнтів
+    """
 
     with open("./data/clients.txt") as clients_file:
         from_file = clients_file.readlines()
 
-    # накопічувач клієнтів
     clients_list = []
-
     for line in from_file:
-        
-        # відрізати '\n' в кінці рядка
-        line = line[:-2]
-        
         line_list = line.split(';')
         clients_list.append(line_list)
-    
+
     return clients_list
 
+
+def get_orders():
+    """повертає список накладних
+
+    Returns:
+        from_file: список накладних
+    """
+    
+    with open('./data/orders.txt') as orders_file:
+        from_file = orders_file.readlines()
+
+    
+    # розбити строку на реквізити та перетворити формати (при потребі)
+    
+    # список-накопичувач
+    orders_list = []    
+    
+    for line in from_file:
+        line_list = line.split(';')
+        line_list[3] = int(line_list[3])
+        line_list[4] = int(line_list[4])
+        orders_list.append(line_list)
+
+    return orders_list
+
+
 def show_clients(clients):
-    """виводить на екран список клієнтів заданого діапазона
+    """виводить список клієнтів по заданому інтервалу кодів
 
     Args:
-        clients ([list]): список клієнтів
+        clients (list): список клієнтів
     """
 
-    client_code_from = input("З якого кода? ")
-    client_code_to   = input("По який код? ") 
+    # задати інтервал виводу
+    client_code_from = input("З якого кода клієнта? ")
+    client_code_to   = input("По який кода клієнта? ")
 
-    kol_lines = 0
+    lines_found = 0
 
     for client in clients:
-        if  client_code_from <= client[0] <= client_code_to:
-            print("код: {:4} назва: {:17} адреса: {:20}".format(client[0], client[1], client[2]))
-            kol_lines += 1
+        if client_code_from <= client[0] <= client_code_to:
+            print ("код: {:5} назва: {:15} адреса: {:25}".format(*client))
+            lines_found += 1
 
-    if kol_lines == 0:
-        print("Код не знайден")
+    if lines_found == 0:
+        print("Клієнтів по Вашому запиту не знайдено") 
 
-clients = get_clients()
-show_clients(clients)
+
+def show_orders(orders):
+    """виводить список накладних на екран
+
+    Args:
+        orders (list): список накладних
+    """
+
+    for order in orders:
+        print("код клієнта: {:3} накладна № {:4} товар: {:20} кількість: {:3} ціна: {:7}"
+            .format(order[0], order[1], order[2], order[3], order[4]))
+
+# clients = get_clients()
+# show_clients(clients)
+
+# orders= get_orders()
+# show_orders(orders)
+
+
--- process_data.py
+++ process_data.py
@@ -2,7 +2,6 @@
 """
 from data_service import get_clients, get_orders
 
-
 # словник в якому будуть накоплюватись результати розрахунків
 zajavka = {
     'oborud' : "",     # назва устаткування
@@ -13,6 +12,53 @@ zajavka = {
     'total'  : 0.0     # сума
 }
 
+def create_zajavka():
+    """формування списку заявок по магазину
 
-print(get_orders())
-print(get_clients())
+    Returns:
+        zajavka_list: список заявок
+    """
+    
+    def get_client_name(client_code):
+        """знаходить назву кліента по коду
+
+        Args:
+            client_code ([type]): код клієнта
+
+        Returns:
+            [type]: назва клієнта
+        """
+        for client in clients:
+            if client_code == client[0]:
+                return client[1]
+            
+        return "*** назва не знайдена"
+    
+    # накопичувач заявок 
+    zajavka_list = []
+
+    orders = get_orders()
+    clients = get_clients()
+
+    for order in orders:
+        
+        # робоча змінна
+        zajavka_work = zajavka.copy()  
+        
+        zajavka_work['oborud']   = order[2]
+        zajavka_work['zakaz']    = order[1]
+        zajavka_work['kol']      = order[3]
+        zajavka_work['price']    = order[4]
+        zajavka_work['total']    = zajavka_work['kol'] * zajavka_work['price']
+        zajavka_work['client']   = get_client_name(order[0])
+        
+        zajavka_list.append(zajavka_work)
+    
+    return zajavka_list
+
+
+
+# zajavkas = create_zajavka()
+
+# for item in zajavkas:
+#     print(item)   
