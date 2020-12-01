e55145b4844e3818c8d884986924999ae864226a
Serhii Shkliarskiy <shkliarskiy@gmail.com>
2020-12-01 10:26:04 +0200

    роботу завершену

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 
 import os
 from process_data import create_zajavka
-
+from data_service import show_clients, show_orders, get_clients, get_orders
 
 MAIN_MENU = \
 """
@@ -55,7 +55,29 @@ def show_zajavka(zajavka_list):
               )
 
     print(FOOTER)
-    
+
+def write_zajavka(zajavka_list):
+    """записує список заявок у текстовий файл
+
+    Args:
+        zajavka_list ([type]): список заявок
+    """
+
+    with open('./data/zajavka.txt', 'w') as zajavka_file:
+        for zajavka in zajavka_list:
+            line = \
+               zajavka['oborud_name'] + ';' +       \
+               zajavka['client_name'] + ';' +       \
+               zajavka['order_number'] + ';' +      \
+               str(zajavka['kol']) + ';' +          \
+               str(zajavka['price']) + ';' +        \
+               str(zajavka['total'])  + '\n' 
+               
+            zajavka_file.write(line)  
+            
+    print('Файл успішно записано ...')             
+                   
+
 while True:
     
     # вивід головного меню
@@ -72,6 +94,22 @@ while True:
         zajavka_list = create_zajavka()
         show_zajavka(zajavka_list)
         input(STOP_MESSAGE)
+        
+    elif command_number == '2':
+        zajavka_list = create_zajavka()
+        write_zajavka(zajavka_list)
+        input(STOP_MESSAGE)
+        
+    elif command_number == '3':
+        orders = get_orders()
+        show_orders(orders)
+        input(STOP_MESSAGE)
+        
+    elif command_number == '4':
+        clients = get_clients()
+        show_clients(clients)
+        input(STOP_MESSAGE)
+
         
     
     
